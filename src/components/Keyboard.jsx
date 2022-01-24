e3b0e7e3d3de38070c0fdacf88381b672cd565e2
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-01-24 11:12:32 -0500

    Small bug fix

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -28,10 +28,10 @@ const Keyboard = (props) => {
             {rowNumber === props.keys.length - 1 ? <button style={{color: props.darkMode ? '#d6d6d6' : 'black'}} className={`${props.darkMode ? 'bg-secondary' : ''} border-1 px-2 rounded keyboardKey`} onClick={() => {props.onEnter()}}>ENTER</button> : null}
             {row.map(
                 (symbol) => 
-                    <button key={symbol} style={{color: props.darkMode ? '#d6d6d6' : 'black'}} className={`${props.darkMode ? 'bg-secondary' : ''} keyboardKey border-1 rounded keyboardLetter text-capitalize`} onClick={() => { props.onType(symbol); this.blur()}}>{symbol}</button>
+                    <button key={symbol} style={{color: props.darkMode ? '#d6d6d6' : 'black'}} className={`${props.darkMode ? 'bg-secondary' : ''} keyboardKey border-1 rounded keyboardLetter text-capitalize`} onClick={() =>  props.onType(symbol)}>{symbol}</button>
                 )}
             {rowNumber === props.keys.length - 1 ? 
-              <button style={{color: props.darkMode ? '#d6d6d6' : 'black'}} className={`keyboardKey ps-2 pe-3 fs-4 border-1 rounded ${props.darkMode ? 'bg-secondary' : ''}`} onClick={() => {props.onBack(); this.blur()}}>
+              <button style={{color: props.darkMode ? '#d6d6d6' : 'black'}} className={`keyboardKey ps-2 pe-3 fs-4 border-1 rounded ${props.darkMode ? 'bg-secondary' : ''}`} onClick={() => props.onBack()}>
                 <FontAwesomeIcon icon={faBackspace}/>
               </button> 
               : null}
